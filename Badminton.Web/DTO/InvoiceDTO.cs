943dba878d7b5b8bfb68b2da886bbd2d6157de78
Trieu Bui Duc <trieubdse170197@fpt.edu.vn>
2024-06-26 16:19:27 +0700

    Invoice(CRUD)

---
+++ Badminton.Web/DTO/InvoiceDTO.cs
@@ -0,0 +1,41 @@
+﻿using Badminton.Web.Models;
+
+namespace Badminton.Web.DTO
+{
+    public class InvoiceDTO
+    {
+        public int UserId { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public decimal? Discount { get; set; }
+
+        public decimal FinalAmount { get; set; }
+
+        public DateTime InvoiceDate { get; set; }
+
+    }
+
+    public class CreateInvoiceDTO
+    {
+        public int UserId { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public decimal? Discount { get; set; }
+
+    }
+
+    public class UpdateInvoiceDTO
+    {
+        public decimal Tax { get; set; }
+
+        public decimal? Discount { get; set; }
+
+        public decimal TotalAmount { get; set; }
+    }
+}

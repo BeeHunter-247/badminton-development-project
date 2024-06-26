a8db6cfaf0de559757b404a4f2960c50d1ed1dde
Trieu Bui Duc <trieubdse170197@fpt.edu.vn>
2024-06-26 16:19:27 +0700

    Invoice(CRUD)

---
+++ Badminton.Web/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using Badminton.Web.DTO;
+using Badminton.Web.Models;
+
+namespace Badminton.Web.Interfaces
+{
+    public interface IInvoiceRepository
+    {
+        Task<List<InvoiceDTO>> GetAll();
+        Task<InvoiceDTO> GetById(int id);
+        Task<Invoice> Create(Invoice invoice);
+        Task<Invoice> Update(int id, UpdateInvoiceDTO invoiceDTO);
+
+    }
+}

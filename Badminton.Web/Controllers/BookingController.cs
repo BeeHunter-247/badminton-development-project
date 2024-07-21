a2dd894f3892d86b8f244db0fa9b0a58ae354932
Trieu Bui Duc <trieubdse170197@fpt.edu.vn>
2024-07-21 23:03:04 +0700

    Cancel Booking

--- Badminton.Web/Controllers/BookingController.cs
+++ Badminton.Web/Controllers/BookingController.cs
@@ -197,7 +197,7 @@ namespace Badminton.Web.Controllers
 
                 // check booking trùng lặp
                 var existingBooking = await _bookingRepo.GetBookingByUserAndTime(bookingDTO.UserId, bookingDTO.SubCourtId, parseBookingDate, bookingDTO.TimeSlotId);
-                if (existingBooking != null)
+                if (existingBooking != null && existingBooking.Status != (int)BookingStatus.Cancelled)
                 {
                     return Ok(new ApiResponse
                     {
@@ -208,7 +208,7 @@ namespace Badminton.Web.Controllers
                 }
 
                 // check SubCourt khả dụng ko
-                var isTimeSlotAvailable = await _bookingRepo.IsTimeSlotAvailableAsync(bookingDTO.SubCourtId, bookingDTO.TimeSlotId, parseBookingDate);
+                var isTimeSlotAvailable = await _bookingRepo.IsIgnoringCancelledAsync(bookingDTO.SubCourtId, bookingDTO.TimeSlotId, parseBookingDate);
                 if (!isTimeSlotAvailable)
                 {
                     return Ok(new ApiResponse
@@ -246,104 +246,6 @@ namespace Badminton.Web.Controllers
             catch
             {
                 return BadRequest();
-            }
-        }
-
-        [HttpPost("create-multiple")]
-        public async Task<IActionResult> CreateMultipleBookingsAsync(CreateMultipleBookingDTO bookingDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(new ApiResponse
-                {
-                    Success = false,
-                    Message = "Invalid data",
-                    Data = ModelState
-                });
-            }
-
-            if (bookingDTO.SubCourtIds == null || bookingDTO.SubCourtIds.Count < 3)
-            {
-                return BadRequest(new ApiResponse
-                {
-                    Success = false,
-                    Message = "You must book at least three sub-courts."
-                });
-            }
-
-            try
-            {
-                if (!DateOnly.TryParse(bookingDTO.BookingDate, out var parseBookingDate))
-                {
-                    ModelState.AddModelError("BookingDate", "Invalid BookingDate format. Please use yyyy-MM-dd.");
-                    return BadRequest(ModelState);
-                }
-
-                var bookings = new List<Booking>();
-                foreach (var subCourtId in bookingDTO.SubCourtIds)
-                {
-                    foreach (var timeSlotId in bookingDTO.TimeSlotIds)
-                    {
-                        // Check for duplicate booking
-                        var existingBooking = await _bookingRepo.GetBookingByUserAndTime(bookingDTO.UserId, subCourtId, parseBookingDate, timeSlotId);
-                        if (existingBooking != null)
-                        {
-                            return Ok(new ApiResponse
-                            {
-                                Success = false,
-                                StatusCode = StatusCodes.Status409Conflict,
-                                Message = $"The user already has a booking for sub-court {subCourtId} on the specified date and time."
-                            });
-                        }
-
-                        // Check if the sub-court is available
-                        var isTimeSlotAvailable = await _bookingRepo.IsTimeSlotAvailableAsync(subCourtId, timeSlotId, parseBookingDate);
-                        if (!isTimeSlotAvailable)
-                        {
-                            return Ok(new ApiResponse
-                            {
-                                Success = false,
-                                StatusCode = StatusCodes.Status409Conflict,
-                                Message = $"SubCourt {subCourtId} is unavailable on the specified date and time."
-                            });
-                        }
-
-                        var booking = new Booking
-                        {
-                            UserId = bookingDTO.UserId,
-                            SubCourtId = subCourtId,
-                            TimeSlotId = timeSlotId,
-                            CreateDate = DateTime.Parse(bookingDTO.CreateDate),
-                            BookingDate = parseBookingDate,
-                            Amount = bookingDTO.Amount,
-                            Status = (int)BookingStatus.Pending,
-                            BookingType = (int)BookingType.Daily,
-                            PromotionCode = string.IsNullOrWhiteSpace(bookingDTO.PromotionCode) || bookingDTO.PromotionCode.Equals("string") ? null : bookingDTO.PromotionCode,
-                        };
-
-                        bookings.Add(booking);
-                    }
-                }
-
-                await _bookingRepo.CreateMultipleAsync(bookings);
-
-                var bookingDTOs = bookings.Select(b => _mapper.Map<BookingDTO>(b)).ToList();
-
-                return Ok(new ApiResponse
-                {
-                    Success = true,
-                    StatusCode = StatusCodes.Status201Created,
-                    Data = bookingDTOs
-                });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new ApiResponse
-                {
-                    Success = false,
-                    Message = "An error occurred while creating the bookings.",
-                    Data = ex.Message
-                });
             }
         }
 

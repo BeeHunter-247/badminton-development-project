6143106dd794441e68cba5aaff9e6ddcb7bfbce5
Duychamchi <duynhon2106@gmail.com>
2024-07-22 23:39:50 +0700

    Top5 user use amost amount
    Top5 user use amost amount

---
+++ Badminton.Web/DTO/User/TopUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Badminton.Web.DTO.User
+{
+    public class TopUserDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
